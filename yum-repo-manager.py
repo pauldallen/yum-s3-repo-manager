c5c5761b6b72eb3100a358a5ac1de408adaf92bf
Paul Allen <paul.allen@lithium.com>
2014-10-15 12:23:53 -0500

    fixing license

--- yum-repo-manager.py
+++ yum-repo-manager.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 
 # Copyright 2014: Lithium Technologies, Inc.
-# License: Restricted
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
 # Author(s):
 #   - Matthew Bogner (matthew.bogner@lithium.com)
 #   - Paul Allen (paul.allen@lithium.com)
